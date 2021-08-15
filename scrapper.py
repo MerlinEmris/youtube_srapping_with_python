857a86f46e721c569685afb752965d343f578b8d
merlinemris <merdanchariyarov@gmail.com>
2021-08-15 17:35:33 +0500

    init

---
+++ scrapper.py
@@ -0,0 +1,7 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+chrome_path = r"C:\Users\ME\projects\for_github\chromedriver_win32\chromedriver.exe"
+driver = webdriver.Chrome(chrome_path)
+
